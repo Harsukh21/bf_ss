80780a707db373fe3daa93728248eaf32a856069
harsukh21 <harsukh21@gmail.com>
2025-11-20 14:34:40 +0530

    filter hieght issue.

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -707,7 +707,7 @@
                         </svg>
                     </div>
                     <div class="ml-3">
-                        <p class="text-sm font-medium text-gray-600 dark:text-gray-400">Total Events</p>
+                        <p class="text-sm font-medium text-gray-600 dark:text-gray-400">All Events</p>
                         <p class="text-xl md:text-2xl font-bold text-gray-900 dark:text-gray-100">{{ number_format($paginatedEvents->total()) }}</p>
                     </div>
                 </div>
